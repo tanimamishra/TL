5c96ebc294fd931dfe224e698f753fa5a42e37c1
tanimamishra <tanimamishra13@gmail.com>
2025-07-28 17:08:28 +0530

    Shape Generator

---
+++ javascript/callBackFunc.js
@@ -0,0 +1,7 @@
+function makingPasta(bread){
+    console.log("making a sandwich with " + bread);
+}
+
+function applySauce(sauce) {
+    console.log("applying ")
+}
---
+++ javascript/script.js
@@ -0,0 +1,31 @@
+// console.log("hello world");
+// console.warm("this is warning");
+
+// var name="vikas";
+// var name="tanima";
+// name=shorya;
+
+// var A = 10;
+// function a() {
+//   var B = 20;
+//   console.log(B);
+
+//   function b() {
+//     var C = 30;
+//     console.log(C);
+
+//     function c() {
+//       var C = 60;
+//       console.log(C);
+//     }
+//     c();
+//   }
+//   b();
+// }
+// a();
+// console.log(A);
+
+
+const a ="tanima"
+console.log(a)
+
